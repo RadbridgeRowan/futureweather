1807d176d542ee146190ae6272d259575c6fd136
RadbridgeRowan <129906364+RadbridgeRowan@users.noreply.github.com>
2025-11-07 11:36:08 +1300

    UI updates

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        index2: resolve(__dirname, 'index2.html'),
       },
     },
   },
